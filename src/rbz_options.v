4787b6c5e52bdc13604c5879b2d78bd38885404c
Anton Maurovic <anton@foogle.com>
2024-11-04 20:49:57 +1030

    Enable NO_DIV_WALLS

--- src/rbz_options.v
+++ src/rbz_options.v
@@ -12,6 +12,7 @@
 //`define RESET_TEXTURE_MEMORY_PATTERNED // If defined with RESET_TEXTURE_MEMORY, texture memory reset is a pattern instead of black.
 //`define DEBUG_NO_TEXTURE_LOAD // If defined, prevent texture loading
 `define NO_EXTERNAL_TEXTURES
+`define NO_DIV_WALLS
 // Use alternate fixed-point config that goes for the minimum (currently) supported by RBZ:
 `define ALT_FIXED_POINT_PARAMS
     `define Qm    9   // Signed. 9 is minimum: Below 9, texv is broken. Below 8, rayAddend overflows.
@@ -19,8 +20,8 @@
     `define Qmnc  18   // <== MUST EQUAL Qmn+Qn. Sort of the same as `Qmn, but that isn't useful for all my Verilog needs.
     // The following matches LZC config (in lzc.v) to the above...
     //NOTE: The FIRST defined of the following will be used:
-    // `define D17 // 17-bit range, e.g. Q8.9
-    `define D18 // 18-bit range, e.g. Q9.9
+    `define D18 // 17-bit range, e.g. Q8.9
+    // `define D18 // 18-bit range, e.g. Q9.9
     // `define D19 // 19-bit range, e.g. Q9.10
     // `define D20 // 20-bit range, e.g. Q10.10
     // `define D22 // 22-bit range, e.g. Q11.11
